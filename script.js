0c7732d5bb2f7362c356c82eb6be41b4352c5590
mudassir8050 <125982472+mudassir8050@users.noreply.github.com>
2023-08-20 21:58:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
-const byteSize = (str) => {
-  // write your code here
-};
-
+// const byteSize = (str) => {
+//   // write your code here
+// };
+function byteSize(str) {
+    const blob = new Blob([str]);
+    return blob.size;
+}
 // Do not change the code below
 const str = prompt("Enter some string.");
 alert(byteSize(str));
+
+
+
+// Test cases
+console.log(byteSize('hello world')); // 11
+console.log(byteSize('안녕하세요')); // 15
+console.log(byteSize('')); // 0
